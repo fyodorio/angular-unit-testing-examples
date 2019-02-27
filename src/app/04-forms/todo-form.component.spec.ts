ef88b7378e78db23747d7d1868980e8f8344b75d
loenko <loenko.js@gmail.com>
2019-02-27 19:45:53 +0500

    Test Angular form controls

--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,11 +1,23 @@
-import { TodoFormComponent } from './todo-form.component';
+import {TodoFormComponent} from './todo-form.component';
+import {FormBuilder} from '@angular/forms';
 
 describe('TodoFormComponent', () => {
-  // const component: TodoFormComponent;
+	let component: TodoFormComponent;
 
-  beforeEach(() => {});
+	beforeEach(() => {
+		component = new TodoFormComponent(new FormBuilder());
+	});
 
-  it('', () => {});
+	it('Should create a form with 2 controls', () => {
+		expect(component.form.contains('name')).toBeTruthy();
+		expect(component.form.contains('email')).toBeTruthy();
+	});
 
-  it('', () => {});
+	it('Should make the name control required', () => {
+		const control = component.form.get('name');
+
+		control.setValue('');
+
+		expect(control.valid).toBeFalsy();
+	});
 });
--- src/app/04-forms/todo-form.component.ts
+++ src/app/04-forms/todo-form.component.ts
@@ -1,12 +1,12 @@
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 
 export class TodoFormComponent {
-  form: FormGroup;
+	form: FormGroup;
 
-  constructor(fb: FormBuilder) {
-    this.form = fb.group({
-      name: ['', Validators.required],
-      email: ['']
-    });
-  }
+	constructor(fb: FormBuilder) {
+		this.form = fb.group({
+			name: ['', Validators.required],
+			email: ['']
+		});
+	}
 }
