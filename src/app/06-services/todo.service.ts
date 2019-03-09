82f1906dcf74933fa60135276d783908a15362e1
loenko <loenko.js@gmail.com>
2019-03-10 01:09:19 +0500

    Test Angular service using spy

--- src/app/06-services/todo.service.ts
+++ src/app/06-services/todo.service.ts
@@ -1,18 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 export class TodoService {
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {
+	}
 
-  add(todo) {
-    // return this.http.post('...', todo).pipe(map(r => r.json()));
-  }
+	add(todo) {
+		return this.http.post('...', todo).pipe(map(r => r.json()));
+	}
 
-  getTodos() {
-    // return this.http.get('...').pipe(map(r => r.json()));
-  }
+	getTodos() {
+		return this.http.get('...').pipe(map(r => r.json()));
+	}
 
-  delete(id) {
-    // return this.http.delete('...').pipe(map(r => r.json()));
-  }
+	delete(id) {
+		return this.http.delete('...').pipe(map(r => r.json()));
+	}
 }
--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,10 +1,28 @@
-import { TodosComponent } from './todos.component';
-import { TodoService } from './todo.service';
+import {TodosComponent} from './todos.component';
+import {TodoService} from './todo.service';
+import {of} from 'rxjs';
 
 describe('TodosComponent', () => {
-  // const component: TodosComponent;
+	let component: TodosComponent;
+	let service: TodoService;
 
-  beforeEach(() => {});
+	beforeEach(() => {
+		service = new TodoService(null);
+		component = new TodosComponent(service);
+	});
 
-  it('', () => {});
+	it('should set todos property with the items returned from the server', () => {
+		const todos = [
+			{id: 1, title: 'a'},
+			{id: 2, title: 'b'},
+			{id: 3, title: 'c'}
+		];
+		spyOn(service, 'getTodos').and.callFake(() => {
+			return of(todos);
+		});
+
+		component.ngOnInit();
+
+		expect(component.todos).toBe(todos);
+	});
 });
