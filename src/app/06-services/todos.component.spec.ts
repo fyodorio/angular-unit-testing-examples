9bac64c592ba0d97f88986b8ca0df5efd7749234
loenko <loenko.js@gmail.com>
2019-03-10 04:48:25 +0500

    Test more Angular service methods

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,6 +1,6 @@
 import {TodosComponent} from './todos.component';
 import {TodoService} from './todo.service';
-import {of} from 'rxjs';
+import {of, throwError} from 'rxjs';
 
 describe('TodosComponent', () => {
 	let component: TodosComponent;
@@ -24,5 +24,35 @@ describe('TodosComponent', () => {
 		component.ngOnInit();
 
 		expect(component.todos).toBe(todos);
+	});
+
+	it('should call the server to save the changes when a new todo item is added', () => {
+		const spy = spyOn(service, 'add').and.callFake(t => {
+			 return of ();
+		});
+
+		component.add();
+
+		expect(spy).toHaveBeenCalled();
+	});
+
+	it('should add the new todo returned from the server', () => {
+		const todo = { id: 1 };
+		const spy = spyOn(service, 'add').and.callFake(t => {
+			return of (todo);
+		});
+
+		component.add();
+
+		expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+	});
+
+	it('should set the message property when server returns an error when adding a new todo', () => {
+		const error = 'error from the server';
+		const spy = spyOn(service, 'add').and.returnValue(throwError(error));
+
+		component.add();
+
+		expect(component.message).toBe(error);
 	});
 });
