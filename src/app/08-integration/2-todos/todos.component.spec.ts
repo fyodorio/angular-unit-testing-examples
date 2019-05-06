43c5ea875d659e3f48ee277d028bcb96311b77f9
loenko <loenko.js@gmail.com>
2019-05-06 13:13:47 +0500

    Integration testing with spy

--- src/app/08-integration/2-todos/todos.component.spec.ts
+++ src/app/08-integration/2-todos/todos.component.spec.ts
@@ -1,34 +1,39 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
-// NOTE: I've deliberately excluded this suite from running
-// because the test will fail. This is because we have not
-// provided the TodoService as a dependency to TodosComponent.
-//
-// When you get to Lecture 6 (Providing Dependencies), be sure
-// to remove "x" from "xdescribe" below.
-
-xdescribe('TodosComponent', () => {
+describe('TodosComponent', () => {
 	let component: TodosComponent;
 	let fixture: ComponentFixture<TodosComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [TodosComponent]
+			imports: [HttpClientModule],
+			declarations: [TodosComponent],
+			providers: [TodoService]
 		}).compileComponents();
 	}));
 
 	beforeEach(() => {
 		fixture = TestBed.createComponent(TodosComponent);
 		component = fixture.componentInstance;
-		fixture.detectChanges();
+		// fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should load todos from the server', () => {
+		const service = TestBed.get(TodoService);
+		// fixture.debugElement.injector.get(TodoService); // - for cases when dependencies aren't provided
+		spyOn(service, 'getTodos').and.returnValue(of([1, 2, 3] ));
+
+		fixture.detectChanges();
+
+		expect(component.todos.length).toBe(3);
 	});
 });
