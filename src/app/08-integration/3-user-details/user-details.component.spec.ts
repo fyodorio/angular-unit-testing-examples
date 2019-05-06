55367215ed7d3c1349fd9d3776967f24dbd17325
loenko <loenko.js@gmail.com>
2019-05-06 14:22:12 +0500

    Test activateRoute using stub and rxjs subject in it

--- src/app/08-integration/3-user-details/user-details.component.spec.ts
+++ src/app/08-integration/3-user-details/user-details.component.spec.ts
@@ -2,14 +2,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDetailsComponent } from './user-details.component';
 import { ActivatedRoute, Router } from '@angular/router';
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY, Observable, Subject } from 'rxjs';
 
 class RouterStub {
 	navigate(params) {}
 }
 
 class ActivatedRouteStub {
-	params: Observable<any> = EMPTY;
+	private subject = new Subject();
+
+	// params: Observable<any> = EMPTY;
+	get params() {
+		return this.subject.asObservable();
+	}
+
+	push(value) {
+		this.subject.next(value);
+	}
 }
 
 describe('UserDetailsComponent', () => {
@@ -45,5 +54,15 @@ describe('UserDetailsComponent', () => {
 		component.save();
 
 		expect(spy).toHaveBeenCalledWith(['users']);
+	});
+
+	it('should navigate the user to the not found page when an invalid user ID is passed', () => {
+		const router = TestBed.get(Router);
+		const spy = spyOn(router, 'navigate');
+		const route: ActivatedRouteStub = TestBed.get(ActivatedRoute);
+
+		route.push({ id: 0 });
+
+		expect(spy).toHaveBeenCalledWith(['not-found']);
 	});
 });
