9da20dc793998b65567dff9546dc93ce66d25bd3
loenko <loenko.js@gmail.com>
2019-05-06 13:27:45 +0500

    Add stubs for angular navigation services

--- src/app/08-integration/3-user-details/user-details.component.spec.ts
+++ src/app/08-integration/3-user-details/user-details.component.spec.ts
@@ -1,17 +1,34 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { UserDetailsComponent } from './user-details.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import {EMPTY, Observable} from 'rxjs';
 
-xdescribe('UserDetailsComponent', () => {
+class RouterStub {
+	navigate(params) {}
+}
+
+class ActivatedRouteStub {
+	params: Observable<any> = EMPTY;
+}
+
+describe('UserDetailsComponent', () => {
 	let component: UserDetailsComponent;
 	let fixture: ComponentFixture<UserDetailsComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [UserDetailsComponent]
+			declarations: [UserDetailsComponent],
+			providers: [
+				{
+					provide: Router,
+					useClass: RouterStub
+				},
+				{
+					provide: ActivatedRoute,
+					useClass: ActivatedRouteStub
+				}
+			]
 		}).compileComponents();
 	}));
 
