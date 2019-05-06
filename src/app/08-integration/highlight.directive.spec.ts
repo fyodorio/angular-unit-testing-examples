55bfb94367f6f03d8c4926df7d231fcde5847f67
loenko <loenko.js@gmail.com>
2019-05-06 15:38:14 +0500

    Test a directive

--- src/app/08-integration/highlight.directive.spec.ts
+++ src/app/08-integration/highlight.directive.spec.ts
@@ -1,13 +1,12 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HighlightDirective } from './highlight.directive';
-import { By } from '@angular/platform-browser';
 import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 @Component({
 	template: `
-		<p highlight="cyan">First</p>
-		<p highlight>Second</p>
+		<p appHighlight="cyan">First</p>
+		<p appHighlight>Second</p>
 	`
 })
 class DirectiveHostComponent {}
@@ -24,5 +23,20 @@ describe('HighlightDirective', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(DirectiveHostComponent);
 		fixture.detectChanges();
+	});
+
+	it('should highlight the first element with cyan', () => {
+		const de = fixture.debugElement.queryAll(By.css('p'))[0];
+
+		expect(de.nativeElement.style.backgroundColor).toBe('cyan');
+	});
+
+	it('should highlight the second element with the default color', () => {
+		const de = fixture.debugElement.queryAll(By.css('p'))[1];
+		const directive = de.injector.get(HighlightDirective);
+
+		expect(de.nativeElement.style.backgroundColor).toBe(
+			directive.defaultColor
+		);
 	});
 });
